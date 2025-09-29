b9ab8ed16eef548a89b4164cfb501a01d406012e
SamSkwanch <slid3rmonk3y@gmail.com>
2025-09-28 20:03:32 -0400

    Rename executable to S-Bux Snagger.exe in build script

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Build S-Bux Snagger as a standalone Windows executable
-pyinstaller --onefile --windowed --add-data "assets\sbux.png;assets" main.py
+pyinstaller --onefile --windowed --name "S-Bux Snagger" --add-data "assets\sbux.png;assets" main.py
 
-REM Output will be in the dist folder
+REM Output will be in the dist folder as S-Bux Snagger.exe
 pause
